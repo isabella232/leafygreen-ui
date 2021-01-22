6f98dba518b2d027bfb6c16fd15aa2850b9845d3
Brooke Yalof <brooke.yalof@mongodb.com>
2021-01-22 13:11:00 -0500

    Updates

--- hosting/files/_next/static/chunks/webpack.js
+++ hosting/files/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5796adb0aa01f6593d8d";
+/******/ 	var hotCurrentHash = "635376500db892a0d0e2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
